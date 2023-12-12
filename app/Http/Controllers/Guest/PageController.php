4bdf310cf6660eebfd2ad6eb71d988c964128306
aSwanting <gab.damico@gmail.com>
2023-12-12 14:52:41 +0100

    add: movie cards from db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('guest.home');
+        $movies = Movie::all();
+        return view('guest.home', compact('movies'));
     }
 }
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 @section('title', 'home sweet home')
-@section('name')
-
+@section('main')
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4 gy-3">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h3>{{ $movie->title }}</h2>
+                                <p>{{ $movie->original_title }}</p>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">{{ $movie->nationality }}</li>
+                                <li class="list-group-item">{{ $movie->date }}</li>
+                                <li class="list-group-item">{{ $movie->vote }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
